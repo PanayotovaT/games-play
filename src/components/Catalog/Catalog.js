2629d035648331b55f42b5a14343d94251b5cd6c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-11-22 23:49:28 +0200

    show no game yet message

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -4,22 +4,28 @@ import GameCard from './GameCard';
 const Catalog = () => {
 
     const [games, setGames] = useState([]);
-
+ 
     useEffect(() => {
+      
         fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
             .then(res => res.json())
             .then(gamesResult => {
                 setGames(gamesResult);
+               
             } );
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
+            
+            { games.length > 0 
 
-            {games.map(game => <GameCard game={game} />)}
+                ? games.map(game => <GameCard key={game._id} game={game} />)
+                :<h3 className="no-articles">No games yet</h3>
+            }
 
-            <h3 className="no-articles">No games yet</h3>
+            
         </section>
 
     );
